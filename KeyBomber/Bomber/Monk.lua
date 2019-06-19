3ff161781de735520d013dbe98592af8d8071d7c
gawrilyako@gmail.com <Konctantin>
2019-06-19 22:59:15 +0300

    Implement dps monk rotation
    Signed-off-by: gawrilyako@gmail.com <Konctantin>

--- KeyBomber/Bomber/Monk.lua
+++ KeyBomber/Bomber/Monk.lua
@@ -9,4 +9,178 @@ BOMBER_MONK_2 = {
 
 -- Windwalker
 BOMBER_MONK_3 = {
+    OnLoad = function()
+    end,
+    {   SpellId =      0, Name = "Инициализация",
+        IsMovingCheck     = "none",
+        RecastDelay       = 0,
+        DropChanel        = false,
+        CancelCasting     = false,
+        IsCheckInCombat   = false,
+        IsUsableCheck     = true,
+        RangeCheck        = false,
+        TargetList = {
+            { Target = "none" }
+        },
+        Func = function(ability, targetInfo, target)
+            BomberFrame.RangeSpell = GetSpellInfo(100780);
+            if IsMounted() then
+                return true;
+            end
+        end
+    },
+    {   SpellId =  137639, Name = "Буря, земля и огонь",
+        IsMovingCheck     = "none",
+        RecastDelay       = 0,
+        DropChanel        = false,
+        CancelCasting     = false,
+        IsCheckInCombat   = true,
+        IsUsableCheck     = true,
+        RangeCheck        = false,
+        TargetList = {
+            { Target = "none" }
+        },
+        Func = function(ability, targetInfo, target)
+            if BOMBER_COOLDOWN
+            and UnitExists("target")
+            and not UnitIsDeadOrGhost("target")
+            and UnitCanAttack("player", "target")
+            and IsSpellInRange(BomberFrame.RangeSpell, "target") == 1
+            and not HasBuff("player", 137639)
+            then
+                return true;
+            end
+        end
+    },
+    {   SpellId =  115080, Name = "Смертельное касание",
+        IsMovingCheck     = "none",
+        RecastDelay       = 0,
+        DropChanel        = false,
+        CancelCasting     = false,
+        IsCheckInCombat   = true,
+        IsUsableCheck     = true,
+        RangeCheck        = false,
+        TargetList = {
+            { Target = "none" }
+        },
+        Func = function(ability, targetInfo, target)
+            if BOMBER_COOLDOWN
+            and UnitExists("target")
+            and not UnitIsDeadOrGhost("target")
+            and UnitCanAttack("player", "target")
+            and IsSpellInRange(BomberFrame.RangeSpell, "target") == 1
+            then
+                return true;
+            end
+        end
+    },
+    {   SpellId =  116705, Name = "Рука-копье",
+        IsMovingCheck     = "none",
+        RecastDelay       = 0,
+        DropChanel        = false,
+        CancelCasting     = false,
+        IsCheckInCombat   = true,
+        IsUsableCheck     = true,
+        RangeCheck        = true,
+        TargetList = {
+            { Target = "target" }
+        },
+        Func = function(ability, targetInfo, target)
+            if CheckInterrupt("target") then
+                return true;
+            end
+        end
+    },
+    {   SpellId =  152175, Name = "Удар крутящегося дракона",
+        IsMovingCheck     = "none",
+        RecastDelay       = 0,
+        DropChanel        = false,
+        CancelCasting     = false,
+        IsCheckInCombat   = true,
+        IsUsableCheck     = true,
+        RangeCheck        = true,
+        TargetList = {
+            { Target = "none" }
+        },
+        Func = function(ability, targetInfo, target)
+            return true;
+        end
+    },
+    {   SpellId =  107428, Name = "Удар восходящего солнца",
+        IsMovingCheck     = "none",
+        RecastDelay       = 0,
+        DropChanel        = false,
+        CancelCasting     = false,
+        IsCheckInCombat   = true,
+        IsUsableCheck     = true,
+        RangeCheck        = true,
+        TargetList = {
+            { Target = "target" }
+        },
+        Func = function(ability, targetInfo, target)
+            return true;
+        end
+    },
+    {   SpellId =  113656, Name = "Неистовые кулаки",
+        IsMovingCheck     = "none",
+        RecastDelay       = 0,
+        DropChanel        = false,
+        CancelCasting     = false,
+        IsCheckInCombat   = true,
+        IsUsableCheck     = true,
+        RangeCheck        = true,
+        TargetList = {
+            { Target = "none" }
+        },
+        Func = function(ability, targetInfo, target)
+            return true;
+        end
+    },
+    {   SpellId =  101546, Name = "Танцующий журавль",
+        IsMovingCheck     = "none",
+        RecastDelay       = 0,
+        DropChanel        = false,
+        CancelCasting     = false,
+        IsCheckInCombat   = true,
+        IsUsableCheck     = true,
+        RangeCheck        = true,
+        TargetList = {
+            { Target = "none" }
+        },
+        Func = function(ability, targetInfo, target)
+            return BOMBER_AOE;
+        end
+    },
+    {   SpellId =  100784, Name = "Нокаутирующий удар",
+        IsMovingCheck     = "none",
+        RecastDelay       = 0,
+        DropChanel        = false,
+        CancelCasting     = false,
+        IsCheckInCombat   = true,
+        IsUsableCheck     = true,
+        RangeCheck        = true,
+        TargetList = {
+            { Target = "target" }
+        },
+        Func = function(ability, targetInfo, target)
+            if HasBuff("player", 116768) or SpellCD(113656) > 4 or UnitPower("player", 12) > 3 then
+                return true;
+            end
+        end
+    },
+    {   SpellId =  100780, Name = "Лапа тигра",
+        IsMovingCheck     = "none",
+        RecastDelay       = 0,
+        DropChanel        = false,
+        CancelCasting     = false,
+        IsCheckInCombat   = true,
+        IsUsableCheck     = true,
+        RangeCheck        = true,
+        TargetList = {
+            { Target = "target" }
+        },
+        Func = function(ability, targetInfo, target)
+            return true;
+        end
+    },
 }
